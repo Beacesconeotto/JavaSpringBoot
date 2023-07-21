af1370aabc206d2f09ff22ff7c205077b756749b
Beacesconetto <beacesconetto203@gmail.com>
2023-07-21 17:03:57 -0300

    feat: post and link to the list

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -40,6 +42,15 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 
 		return "employees/employee-form";
+	}
+	@PostMapping("/save")
+	public String saveEmpployee(@ModelAttribute("employee") Employee theEmployee) {
+
+		//save the employee
+		employeeService.save(theEmployee);
+
+		//use a redirect to prevent duplicate subimissions
+		return "redirect:/employees/list";
 	}
 }
 
